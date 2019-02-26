bdd9fb8991c5ddb6c00118b9336d5f8b9c1ff547
aammarr <aammar.ahmed>
2019-02-26 23:03:02 +0500

    ....

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -53,7 +53,7 @@ class Authenticate
 
         $access_token = $request->header('Authorization');
         $access_token = str_replace("Bearer ","",$access_token);
-        dd("hh",$request,$access_token);
+        dd("hh",$request->header,$access_token);
         if($access_token){
             $user = User::where('access_token',$access_token)->first();
 
